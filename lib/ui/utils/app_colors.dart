555d88a59aca8a3d746ac4ba2c28b5206d6c24c2
Ahmed Nabil <ahmed.nabil@kazyon.com>
2025-12-14 20:30:06 +0200

    Revert "Update app colors."
    This reverts commit 33cc3b60c7a84a1d57c5b26db712a38870a85bff.

--- lib/ui/utils/app_colors.dart
+++ lib/ui/utils/app_colors.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
 
 abstract final class AppColors {
-  static const Color lightBlue = Color(0xffd62525);
-  static const Color blue = Color(0xff6992ea);
+  static const Color lightBlue = Color(0xff00D2FF);
+  static const Color blue = Color(0xff3A7BD5);
   static const Color white = Colors.white;
   static const Color black = Colors.black;
 }
